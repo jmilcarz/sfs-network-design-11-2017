eb3aceb30112497ab76c654a82591e9b980f50e8
kmilcarz <j.milcarz@yahoo.com>
2017-11-09 15:27:40 +0100

    navigation for mobile

--- js/functions.js
+++ js/functions.js
@@ -14,4 +14,34 @@ $(document).ready(function() {
      if ($("#searchboxfield").attr("val") != "") {
           $("#searchresults").css("display", "block");
      }
+
+     // mobile new post modal
+
+     $("#toogleMobileNewPost").click(function() {
+          $("#mobileNewPostModal").slideToggle(800, function() {
+               $("#mobileNewPostModal").css("display", "block");
+          });
+     });
+
+     $("#closeMobileNewPost").click(function() {
+          $("#mobileNewPostModal").slideToggle(800, function() {
+               $("#mobileNewPostModal").css("display", "none");
+          });
+     });
+
+     // mobile searchbox
+
+     $("#tooglesearchbox-mobile").click(function() {
+          $("#searchbox-mobile").slideToggle(180, function() {
+               $("#searchresults-mobile").css("display", "block");
+               $("#searchbox-mobile").css("display", "block");
+          });
+     });
+
+     $("#searchboxclose-mobile").click(function() {
+          $("#searchbox-mobile").slideToggle(800, function() {
+               $("#searchresults-mobile").css("display", "none");
+               $("#searchbox-mobile").delay(500).css("display", "none");
+          });
+     });
 });
